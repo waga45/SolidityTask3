dcbc75e58b87e342505af7699c6fe0169105c91e
root <admin@example.com>
2025-09-20 20:45:59 +0800

    first

---
+++ contracts/SunNFT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+//SunNFT
+contract SunNFT is Initializable,ERC721Upgradeable,ERC721URIStorageUpgradeable,UUPSUpgradeable,OwnableUpgradeable{
+
+
+}
